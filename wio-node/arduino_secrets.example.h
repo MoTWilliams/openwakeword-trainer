6de5f0ea2684877d701227c0c877bd99ccc256cc
Mo <MoTaylorWilliams@gmail.com>
2026-06-22 00:02:01 -0500

    Built preliminary file structure

---
+++ wio-node/arduino_secrets.example.h
@@ -0,0 +1,12 @@
+#ifndef ARDUINO_SECRETS_H
+#define ARDUINO_SECRETS_H
+
+#include <Arduino.h>
+
+const char* SSID = "Your SSID";
+const char* PASSWORD = "Your password";
+
+const char* HOST = "1.2.3.4";
+const uint16_t PORT = 8080;
+
+#endif
